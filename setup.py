4278a9265ede3952f38b9a91d2cc521998002810
gabrinovas <gabriel.novas@aimen.es>
2025-10-09 23:06:47 +0200

    Delete legacy files

--- setup.py
+++ setup.py
@@ -4,19 +4,21 @@ package_name = 'onrobot_2fg7_gripper_description'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='1.0.0',  # Match your package.xml version
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # Add these to ensure all files are installed:
+        ('share/' + package_name, ['setup.cfg']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='gabri',
     maintainer_email='gabri@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='URDF description and meshes for OnRobot 2FG7 gripper',  # Updated description
+    license='Apache-2.0',  # Match your license
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
